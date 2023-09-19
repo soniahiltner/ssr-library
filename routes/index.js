6966b5f1dc65579ab7f8c4da6ce0bfcd6bc92cfb
Sonia <sonia.hiltner@gmail.com>
2023-09-19 23:02:18 +0200

    Add quote

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
+const Quote = require('../models/quote')
 
 // Get last 10 books
 router.get('/', async (req, res) => {
-  let books, count
+  let books, quote
   try {
     books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
-  } catch {
+    const count = await Quote.countDocuments().exec()
+    const random = Math.floor(Math.random() * count)
+    quote = await Quote.findOne().skip(random).exec()
+  } catch (err) {
     books = []
+    console.log(err)
   }
-  res.render('index', { books: books })
+  res.render('index', { books: books, quote: quote })
 })
 
 module.exports = router
